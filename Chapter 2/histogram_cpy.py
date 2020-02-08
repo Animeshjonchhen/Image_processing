7ce2d57cd53520e21074a82d84e638d6fe9939df
ANIMESH JONCHHEN <animeshjonchhen123@gmail.com>
2020-02-08 15:10:36 +0545

    add

--- Chapter 2/histogram_cpy.py
+++ Chapter 2/histogram_cpy.py
@@ -13,6 +13,7 @@ plt.show()
 equ = cv2.equalizeHist(img)
 plt.hist(equ.ravel(), 256, [0, 256])
 plt.show()
+
 res = np.hstack((img, equ))  # stacking images side-by-side
 cv2.imshow('result image', equ)
 cv2.waitKey(1000000)
